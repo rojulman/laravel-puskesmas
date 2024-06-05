9c90326e0f627ea1cd12d35b709744057d9cc49e
Sirojul Munir <rojulman@gmail.com>
2024-06-05 08:41:42 +0700

    add routing kelurahan

--- routes/web.php
+++ routes/web.php
@@ -7,13 +7,20 @@ use App\Http\Controllers\PasienController;
 Route::get('/pasien/show',[PasienController::class, 'show'])->name('pasien.show');
 Route::get('/pasien/create',[PasienController::class, 'create'])->name('pasien.create');
 Route::post('/pasien/store',[PasienController::class, 'store'])->name('pasien.store');
-Route::get('/pasien/{id}',[PasienController::class, 'view'])->name('pasien.view');
+Route::get('/pasien/{id}/view',[PasienController::class, 'view'])->name('pasien.view');
 Route::get('/pasien/{id}/edit',[PasienController::class, 'edit'])->name('pasien.edit');
 Route::delete('/pasien/{id}',[PasienController::class, 'destroy'])->name('pasien.destroy');
+Route::get('/pasien/kelurahan',[PasienController::class, 'kelurahan'])->name('pasien.kelurahan');
+
+use App\Http\Controllers\KelurahanController;
+Route::get('/kelurahan/show',[KelurahanController::class, 'show'])->name('kelurahan.show');
+Route::get('/kelurahan/{id}/view',[KelurahanController::class, 'view'])->name('kelurahan.view');
+
 
 use App\Http\Controllers\ParamedikController;
 Route::get('/paramedik/show',[ParamedikController::class, 'show'])->name('paramedik.show');
 
 use App\Http\Controllers\AdminController;
 Route::get('/admin', [AdminController::class, 'index']);
+
 
